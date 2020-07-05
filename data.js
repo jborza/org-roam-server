c1f60cd2c60919bd2c334e5c12d05ad8d15067d0
Juraj Borza <jborza@gmail.com>
2020-07-05 18:47:22 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,190 @@
+var data = {
+    "nodes": [
+        {
+            "id": "20200705181611-humans",
+            "title": "humans",
+            "tags": null,
+            "label": "humans",
+            "url": "org-protocol://roam-file?file=%2Fhome%2Fjuraj%2Forg-roam%2F20200705181611-humans.org",
+            "path": "/home/juraj/org-roam/20200705181611-humans.org"
+        },
+        {
+            "id": "20200705180953-dog_poop",
+            "title": "dog poop",
+            "tags": null,
+            "label": "dog poop",
+            "url": "org-protocol://roam-file?file=%2Fhome%2Fjuraj%2Forg-roam%2F20200705180953-dog_poop.org",
+            "path": "/home/juraj/org-roam/20200705180953-dog_poop.org"
+        },
+        {
+            "id": "20200705180918-cow_poop",
+            "title": "cow poop",
+            "tags": null,
+            "label": "cow poop",
+            "url": "org-protocol://roam-file?file=%2Fhome%2Fjuraj%2Forg-roam%2F20200705180918-cow_poop.org",
+            "path": "/home/juraj/org-roam/20200705180918-cow_poop.org"
+        },
+        {
+            "id": "20200705180827-reptile_poop",
+            "title": "reptile poop",
+            "tags": null,
+            "label": "reptile poop",
+            "url": "org-protocol://roam-file?file=%2Fhome%2Fjuraj%2Forg-roam%2F20200705180827-reptile_poop.org",
+            "path": "/home/juraj/org-roam/20200705180827-reptile_poop.org"
+        },
+        {
+            "id": "20200705180646-adults",
+            "title": "adults",
+            "tags": null,
+            "label": "adults",
+            "url": "org-protocol://roam-file?file=%2Fhome%2Fjuraj%2Forg-roam%2F20200705180646-adults.org",
+            "path": "/home/juraj/org-roam/20200705180646-adults.org"
+        },
+        {
+            "id": "20200705180606-adult_poop",
+            "title": "adult poop",
+            "tags": null,
+            "label": "adult poop",
+            "url": "org-protocol://roam-file?file=%2Fhome%2Fjuraj%2Forg-roam%2F20200705180606-adult_poop.org",
+            "path": "/home/juraj/org-roam/20200705180606-adult_poop.org"
+        },
+        {
+            "id": "20200705180526-babies",
+            "title": "babies",
+            "tags": null,
+            "label": "babies",
+            "url": "org-protocol://roam-file?file=%2Fhome%2Fjuraj%2Forg-roam%2F20200705180526-babies.org",
+            "path": "/home/juraj/org-roam/20200705180526-babies.org"
+        },
+        {
+            "id": "20200705180424-baby_poop",
+            "title": "baby poop",
+            "tags": null,
+            "label": "baby poop",
+            "url": "org-protocol://roam-file?file=%2Fhome%2Fjuraj%2Forg-roam%2F20200705180424-baby_poop.org",
+            "path": "/home/juraj/org-roam/20200705180424-baby_poop.org"
+        },
+        {
+            "id": "20200705180136-human_poop",
+            "title": "human poop",
+            "tags": null,
+            "label": "human poop",
+            "url": "org-protocol://roam-file?file=%2Fhome%2Fjuraj%2Forg-roam%2F20200705180136-human_poop.org",
+            "path": "/home/juraj/org-roam/20200705180136-human_poop.org"
+        },
+        {
+            "id": "20200705175800-poops",
+            "title": "poops",
+            "tags": null,
+            "label": "poops",
+            "url": "org-protocol://roam-file?file=%2Fhome%2Fjuraj%2Forg-roam%2F20200705175800-poops.org",
+            "path": "/home/juraj/org-roam/20200705175800-poops.org"
+        },
+        {
+            "id": "20200705175238-avian_poop",
+            "title": "avian poop",
+            "tags": null,
+            "label": "avian poop",
+            "url": "org-protocol://roam-file?file=%2Fhome%2Fjuraj%2Forg-roam%2F20200705175238-avian_poop.org",
+            "path": "/home/juraj/org-roam/20200705175238-avian_poop.org"
+        },
+        {
+            "id": "machine_learning",
+            "title": "Machine Learning",
+            "tags": null,
+            "label": "Machine Learning",
+            "url": "org-protocol://roam-file?file=%2Fhome%2Fjuraj%2Forg-roam%2Fmachine_learning.org",
+            "path": "/home/juraj/org-roam/machine_learning.org"
+        },
+        {
+            "id": "20200705173047-",
+            "title": "poop memories",
+            "tags": null,
+            "label": "poop memories",
+            "url": "org-protocol://roam-file?file=%2Fhome%2Fjuraj%2Forg-roam%2F20200705173047-.org",
+            "path": "/home/juraj/org-roam/20200705173047-.org"
+        }
+    ],
+    "edges": [
+        {
+            "from": "20200705180646-adults",
+            "to": "20200705181611-humans",
+            "arrows": null
+        },
+        {
+            "from": "20200705180526-babies",
+            "to": "20200705181611-humans",
+            "arrows": null
+        },
+        {
+            "from": "20200705173047-",
+            "to": "20200705181611-humans",
+            "arrows": null
+        },
+        {
+            "from": "20200705175800-poops",
+            "to": "20200705180953-dog_poop",
+            "arrows": null
+        },
+        {
+            "from": "20200705175800-poops",
+            "to": "20200705180918-cow_poop",
+            "arrows": null
+        },
+        {
+            "from": "20200705175800-poops",
+            "to": "20200705180827-reptile_poop",
+            "arrows": null
+        },
+        {
+            "from": "20200705180606-adult_poop",
+            "to": "20200705180646-adults",
+            "arrows": null
+        },
+        {
+            "from": "20200705180526-babies",
+            "to": "20200705180646-adults",
+            "arrows": null
+        },
+        {
+            "from": "20200705180136-human_poop",
+            "to": "20200705180606-adult_poop",
+            "arrows": null
+        },
+        {
+            "from": "20200705180424-baby_poop",
+            "to": "20200705180526-babies",
+            "arrows": null
+        },
+        {
+            "from": "20200705180136-human_poop",
+            "to": "20200705180424-baby_poop",
+            "arrows": null
+        },
+        {
+            "from": "20200705175800-poops",
+            "to": "20200705180424-baby_poop",
+            "arrows": null
+        },
+        {
+            "from": "20200705175800-poops",
+            "to": "20200705180136-human_poop",
+            "arrows": null
+        },
+        {
+            "from": "20200705175238-avian_poop",
+            "to": "20200705180136-human_poop",
+            "arrows": null
+        },
+        {
+            "from": "20200705175238-avian_poop",
+            "to": "20200705175238-avian_poop",
+            "arrows": null
+        },
+        {
+            "from": "20200705173047-",
+            "to": "machine_learning",
+            "arrows": null
+        }
+    ]
+};
